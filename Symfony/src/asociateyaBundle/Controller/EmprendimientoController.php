427793295e7e09fd5ca1e860130a9560da270520
Federico N Croci <fedecroci@hotmail.com>
2015-10-31 01:24:52 -0300

    Agregado distinct en consulta para listado emprendimientos

--- Symfony/src/asociateyaBundle/Controller/EmprendimientoController.php
+++ Symfony/src/asociateyaBundle/Controller/EmprendimientoController.php
@@ -124,7 +124,13 @@ class EmprendimientoController extends Controller
 
         $entities = $em->getRepository('asociateyaBundle:Emprendimiento')->findByEmprendedor($this->getUser()->getEmprendedor());
 
-        $participo = $em->getRepository('asociateyaBundle:Inversion')->findByUsuario($this->getUser());
+        $participo = $em->getRepository('asociateyaBundle:Inversion')
+            ->createQueryBuilder('e')
+            ->where('e.usuario = :usuario') 
+            ->setParameter('usuario',$this->getUser())
+            ->groupBy('e.emprendimiento')
+            ->getQuery()
+            ->getResult();
 
         return $this->render('asociateyaBundle:Emprendimiento:listado.html.twig', array(
             'entities' => $entities,
