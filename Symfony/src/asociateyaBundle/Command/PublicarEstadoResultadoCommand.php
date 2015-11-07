649fa3122c1e3550a05afb915b881f844e307790
Federico Croci <fedecroci@hotmail.com>
2015-11-07 12:00:59 -0300

    comando

--- Symfony/src/asociateyaBundle/Command/PublicarEstadoResultadoCommand.php
+++ Symfony/src/asociateyaBundle/Command/PublicarEstadoResultadoCommand.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use asociateyaBundle\Controller\mercadopago;
  
 class PublicarEstadoResultadoCommand extends ContainerAwareCommand
 {
@@ -13,7 +14,7 @@ class PublicarEstadoResultadoCommand extends ContainerAwareCommand
         $this->setName('asociateya:publicarEstadoResultado')
             ->setDescription('Notifica y paga ganancias a inversores')
             ->addArgument('emprendimiento', InputArgument::OPTIONAL, 'Publicar ganancias de un emprendimiento especifico.')
-            ->addArgument('ganancias', InputArgument::OPTIONAL, 'Monto de las ganancias.');
+            ->addArgument('ganancias', InputArgument::OPTIONAL, 'Porcentaje de ganancias a pagar.');
     }
  
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -31,9 +32,36 @@ class PublicarEstadoResultadoCommand extends ContainerAwareCommand
         }
 
  
-        // Do whatever
+  
 
         //recorrer inversiones
+
+        $inversiones = $emprendimiento->getInversiones();
+
+
+        $mp = new \MP ("813635953433843", "42DSugNu5tAKsQMj6QicKloh6Jvege3D");
+
+
+        foreach ($inversiones as $inversion) {
+                
+            $montoAPagar = (float)$inversion->getCantidadAcciones()*(float)$emprendimiento->getPrecioAccion()*(0.0495);
+
+            //TODO completar bien la info del pago
+$output->writeln("<info>Se pagaran ganacias a ".." </info>");
+                $payment_data = array(
+                    "transaction_amount" => $montoAPagar,
+                    "token" => "42DSugNu5tAKsQMj6QicKloh6Jvege3D",
+                    "description" => "Ganancias de emprendimiento ".$emprendimiento->getNombre() ,
+                    "installments" => 1,
+                    "payer" => array ("id" => "12345678"),
+                    "payment_method_id" => "visa",
+                    "application_fee" => 0
+                    );
+
+                //$payment = $mp->post("/v1/payments", $payment_data);
+
+        }
+
 
         //emitir pago
 
